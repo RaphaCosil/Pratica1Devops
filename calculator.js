3c17c5c65e54e9b23ca5eb9c83df4a83198ac471
RaphaCosil <Raphael.Cosil@gmail.com>
2024-02-21 11:50:33 -0300

    Adicionando arquivo js com métodos da calculadora

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function soma(x, y){
+    return x+y
+}
+function subtracao(x, y){
+    return x-y
+}
+function multiplicacao(x, y){
+    return x*y
+}
+function divisao(x, y){
+    return x/y
+}
+
+soma(1, 2)
+subtracao(3,1)
+multiplicacao(5,2)
+divisao(6,3)
